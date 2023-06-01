ce793eed57a2e2ed90c5c50afbd533b167eae2c7
Linnea2058 <Linnea2058@gmail.com>
2023-06-01 15:19:37 +0800

    搭建开发环境
    （1）数据库搭建：借住 navicat 创建数据库表
    （2）maven项目环境搭建：pom文件导入依赖、插件和父工程等；yaml文件配置端口、数据库连接信息；创建启动类ReggieApplication；加入静态资源并在配置类（config/WebMvcConfig）中配置静态资源映射

---
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -0,0 +1,16 @@
+package com.itheima.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//日志
+@Slf4j
+//SpringBoot启动类的注解
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+        log.info("项目启动成功...");
+    }
+}

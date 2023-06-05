18dd1f3685f173c61dce829bb270a41e25d07fa0
Linnea2058 <Linnea2058@gmail.com>
2023-06-05 14:46:33 +0800

    分页查询 菜品分类表

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -1,14 +1,14 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -18,10 +18,42 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
     @PostMapping
     public R<String> save(@RequestBody Category category){
         log.info("新增分类{}",category);
         categoryService.save(category);
         return R.success("新增分类成功");
+    }
+
+
+    /**
+     * 分页
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        log.info("page={},pageSize={}",page, pageSize);
+
+        //构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        //条件构造器
+        LambdaQueryWrapper<Category> lambdaQueryWrapper = new LambdaQueryWrapper();
+
+        //根据sort排序
+        lambdaQueryWrapper.orderByDesc(Category::getSort);
+
+        //执行查询
+        categoryService.page(pageInfo, lambdaQueryWrapper);
+
+        return R.success(pageInfo);
     }
 }

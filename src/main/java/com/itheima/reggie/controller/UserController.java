0854ca73379343fc841fe93745809518196efc15
Linnea2058 <Linnea2058@gmail.com>
2023-06-09 11:18:02 +0800

    客户登录功能准备工作
    user实体类、mapper、service、serviceImpl、UserController;
    导入工具类：SMSUtils（短信发送工具类）、ValidateCodeUtils（随机生成验证码工具类）；
    导入阿里云的两个包：aliyun-java-sdk-core、aliyun-java-sdk-dysmsapi

---
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.itheima.reggie.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 客户登录
+ */
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+
+}
